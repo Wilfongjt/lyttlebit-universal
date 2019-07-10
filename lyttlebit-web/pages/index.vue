71d59ba851413961e9e159404d484231044c1a32
James <wilfongjt@gmail.com>
2019-07-10 07:38:04 -0400

    lyttlebit-web-init

---
+++ lyttlebit-web/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <Header />
+    <Home />
+    <Footer />
+  </section>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Home from '@/components/Home.vue'
+import Footer from '@/components/Footer.vue'
+// import Why from '@/components/Why.vue'
+export default {
+  components: {
+    Header,
+    Home,
+    Footer
+  }
+}
+</script>
