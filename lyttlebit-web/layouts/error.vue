bb4a0dc8137b436000d34a1ec5fd312f4f851e53
James <wilfongjt@gmail.com>
2019-08-10 14:38:23 -0400

    lyttlebit-web-init

---
+++ lyttlebit-web/layouts/error.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- div id="search" class="center" -->
+  <div class="band">
+    <h1>...</h1>
+    <h1 v-if="error.statusCode === 404">
+      Page not found
+    </h1>
+    <h1 v-else>
+      An error occurred
+    </h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+    <h1>...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'blog' // you can set a custom layout for the error page
+}
+</script>
+<style scoped>
+.band {
+  width: 100%;
+}
+header {
+  width: 100%;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 30px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+.subtitle {
+  font-weight: 300;
+  font-size: 21px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+.center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+</style>
